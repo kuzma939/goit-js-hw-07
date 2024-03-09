bf88ba263bcd06112d504d6c0b7389cda13dde6a
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-03-09 08:35:44 +0100

    goit-js-hw-07

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,12 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
-
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+let items = document.querySelectorAll('li.item');
+    console.log(`categories: ${items.length}`);
+    items.forEach(item => {
+        showCategoryInfo(item);
+    });
+    function showCategoryInfo(category) {
+        let categoryTitle = category.querySelector('h2').textContent;
+        let categoryElementsCount = category.querySelectorAll('li').length;
+        console.log(`category: ${categoryTitle}`);
+        console.log(`elements: ${categoryElementsCount}`);
+        
+    }
