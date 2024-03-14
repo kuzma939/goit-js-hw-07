11afab92256d9d7ea005cf3b7c7b5d728246ab0d
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-03-14 12:30:45 +0100

    goit-js-hw-07

--- js/task-6.js
+++ js/task-6.js
@@ -1,37 +1,41 @@
+
+
+const refs = {
+  amountEl: document.querySelector('#controls input'),
+  createEl: document.querySelector('button[data-create]'),
+  destroyEl: document.querySelector('button[data-destroy]'),
+  boxesEl: document.querySelector('#boxes'),
+};
+refs.createEl.addEventListener('click', onCreateClick);
+refs.destroyEl.addEventListener('click', onDestroyClick);
+function onCreateClick() {
+  let amount = refs.amountEl.value;
+  if (amount < 1 || amount > 100) {
+    return alert(
+      'Oops, something go wrong!!! The value must be less than 100'
+    );
+  }
+  onDestroyClick();
+  createBoxes(amount);
+}
+function onDestroyClick() {
+  refs.boxesEl.innerHTML = '';
+  refs.amountEl.value = '';
+}
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    let elem = document.createElement('div');
+    elem.style.width = 30 + 10 * i + 'px';
+    elem.style.height = 30 + 10 * i + 'px';
+    elem.style.backgroundColor = getRandomHexColor();
+    refs.boxesEl.append(elem);
+  }
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-const numberElements = document.querySelector("input");
-const addingCollection = document.querySelector("[data-create]");
-const oldCollection = document.querySelector("[data-destroy]");
-const cleaningCollection = document.querySelector("#boxes");
-
-addingCollection.addEventListener("click", createBoxes);
-function createBoxes() {
-  let width = 30;
-let height = 30;
-  cleaningCollection.innerHTML = ''; 
-  const amount = numberElements.value;
-    for(let i = 0; i < amount; i++)
-    {
-     const cub = document.createElement("div");
-cub.style.width = `${width + i * 10}px`;
-cub.style.height = `${height + i * 10}px`;
-cub.style.backgroundColor = getRandomHexColor();
-cleaningCollection.append(cub);
-
-
-}
-numberElements.value = "";
-};
-oldCollection.addEventListener("click", destroyBoxes);
-function destroyBoxes() {
-cleaningCollection.innerHTML = ""; 
-};
-
-
 
 
 
